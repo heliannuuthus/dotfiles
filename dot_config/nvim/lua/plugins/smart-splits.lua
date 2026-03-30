ae8f781884f767e37cf3228fd08103b3b26b6d7d
heliannuuthus <heliannuuthus@gmail.com>
2026-03-30 10:48:28 +0800

    feat: 添加 kitty 插件和 Neovim 最小配置
    - kitty.conf 转为 chezmoi 模板，按平台区分透明度和快捷键
      - macOS: ⌘ 风格快捷键 + Natural Text Editing + 不透明背景
      - Linux: Ctrl+Shift 风格快捷键 + 半透明背景
    - 添加 kitty 插件:
      - kitty-scrollback.nvim: 用 Neovim 浏览终端滚动历史
      - kitty-kitten-search: 终端历史增量搜索
      - smart-splits.nvim: Neovim ↔ Kitty 窗格无缝导航
    - 添加 Neovim 最小配置 (lazy.nvim + kitty 插件)
    - 启用 kitty remote control (smart-splits 需要)
    - 添加 macos_titlebar_color background

---
+++ dot_config/nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,20 @@
+return {
+  "mrjones2014/smart-splits.nvim",
+  lazy = false,
+  config = function()
+    local ss = require("smart-splits")
+    ss.setup({
+      at_edge = "stop",
+    })
+
+    vim.keymap.set("n", "<C-h>", ss.move_cursor_left)
+    vim.keymap.set("n", "<C-j>", ss.move_cursor_down)
+    vim.keymap.set("n", "<C-k>", ss.move_cursor_up)
+    vim.keymap.set("n", "<C-l>", ss.move_cursor_right)
+
+    vim.keymap.set("n", "<A-h>", ss.resize_left)
+    vim.keymap.set("n", "<A-j>", ss.resize_down)
+    vim.keymap.set("n", "<A-k>", ss.resize_up)
+    vim.keymap.set("n", "<A-l>", ss.resize_right)
+  end,
+}
